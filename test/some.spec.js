9d90de20cec6ba153d1e9ddee147e1d358d4c8f9
Vegasok <lamps23@mail.ru>
2016-02-11 15:00:21 +0300

    karma

--- test/some.spec.js
+++ test/some.spec.js
@@ -1,6 +1,6 @@
 describe('http', function () {
 
   it('http', function () {
-    expect(http('http://api.fixer.io/latest').toEqual('http://api.fixer.io/latest');
+    expect(http().toEqual();
   });
 });
